3a7efc3e00b53d8ce811789eccaf2f6070e2f2dc
amit <amitchauhan4@gmail.com>
2019-04-06 21:25:50 +0530

    added multiple sms using array of objects

--- index.js
+++ index.js
@@ -13,19 +13,50 @@ import request from "request";
 */
 // we will take unicode default 0 in case if you don't want to send message in local language, if you want to send message in local language you must pass the unicode value as 1.
 
-export const sendSms = (options) => {
-  const {authkey,number,message,senderID,route,dialCode,unicode = 0} = options;
-  return new Promise((resolve, reject) => {
-    const url = `http://api.msg91.com/api/sendhttp.php?authkey=authkey=${authkey}&mobiles=${number}&message=${message}&sender=${senderID}&route=${route}&country=${dialCode}&unicode=${unicode}`;
-    //encoding the url, in case of local language you can only send up to 700 character long message due to header size but for english character you can send up to 3k+ character long message.
-    var encodeurl=encodeURI(url);
-    request(encodeurl, function (err, response, body) {
-      if(err){
-        console.log(err);
-        reject(err);
-        return;
-      }
-      resolve(body);
-    });
-  });
+export const sendSms = async (optionArray): Promise<any> => {
+  // if you wish to send multiple sms or you want to send single sms just pass the option parameter in array and rest it will look.
+  try {
+    optionArray.forEach((options, index) => {
+      const {authkey,number,message,senderID,route,dialCode,unicode = 0} = options;
+      return new Promise((resolve, reject) => {
+        const url = `http://api.msg91.com/api/sendhttp.php?authkey=authkey=${authkey}&mobiles=${number}&message=${message}&sender=${senderID}&route=${route}&country=${dialCode}&unicode=${unicode}`;
+        //encoding the url, in case of local language you can only send up to 700 character long message due to header size but for english character you can send up to 3k+ character long message.
+        var encodeurl=encodeURI(url);
+        request(encodeurl, (err, response, body) => {
+          if(err){
+            return err;
+          }
+          return body;
+        })
+      });
+    })
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+// Here this function will be execute to generate the data and calling main sms function.
+const smsGenerator = async () => {
+  const data = [
+    {
+      authkey: `XXXXXXXXXXX`,
+      number: `XXXXXXXXXXX`,
+      message : `testing purposes`,
+      senderID : `XXXXXXXXXX`,
+      route : `4`,
+      dialCode : `+91`,
+      unicode : 0
+    },
+    {
+      authkey: `XXXXXXXXXXX`,
+      number: `XXXXXXXXXXX`,
+      message : `testing purposes`,
+      senderID : `XXXXXXXXXX`,
+      route : `1`,
+      dialCode : `+91`,
+      unicode : 1
+    }
+  ];
+  const sendSmsFunction = await sendSms(data);
 }
